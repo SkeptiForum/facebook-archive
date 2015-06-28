d5ce6385e6b8fa99c23e8199030b6dbad28a49d0
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-06-27 17:52:43 -0700

    Added initial documentation for Angular files

--- Website/Shared/Scripts/app.routes.js
+++ Website/Shared/Scripts/app.routes.js
@@ -1,23 +1,43 @@
-﻿angular
+﻿(function () {
+  'use strict';
+
+/*==============================================================================================================================
+| Author        Jeremy Caney (Jeremy@Caney.net)
+| Client        Skepti-Forum Project
+| Project       Facebook Archive
+\=============================================================================================================================*/
+  angular
   .module('app')
   .config(['$routeProvider', configureRoutes]);
 
-function configureRoutes($routeProvider) {
-  $routeProvider
-    .when('/groups', {
-      templateUrl: '/Groups/Index.html',
-    })
-    .when('/groups/:groupId', {
-      templateUrl: '/Threads/Index.html',
-      controller: 'ThreadIndexController',
-      controllerAs: 'vm'
-    })
-    .when('/groups/:groupId/permalink/:postId', {
-      templateUrl: '/Threads/Detail.html',
-      controller: 'ThreadDetailController',
-      controllerAs: 'vm'
-    })
-    .otherwise({
-      redirectTo: '/groups'
-    });
-}
+  configureRoutes.$inject = ['$routeProvider'];
+
+/*==============================================================================================================================
+| CONFIGURE ROUTES
+\-----------------------------------------------------------------------------------------------------------------------------*/
+/** @name app:configureRoutes
+  * @description
+  * Configures routes associated with the Angular application. These are intended for use in a single-page application (SPA), 
+  * although the website currently operates as a multi-page application with pre-rendering performed on the server-side. 
+  */
+  function configureRoutes($routeProvider) {
+    $routeProvider
+      .when('/groups', {
+        templateUrl: '/Groups/Index.html',
+      })
+      .when('/groups/:groupId', {
+        templateUrl: '/Threads/Index.html',
+        controller: 'ThreadIndexController',
+        controllerAs: 'vm'
+      })
+      .when('/groups/:groupId/permalink/:postId', {
+        templateUrl: '/Threads/Detail.html',
+        controller: 'ThreadDetailController',
+        controllerAs: 'vm'
+      })
+      .otherwise({
+        redirectTo: '/groups'
+      });
+  }
+
+})();
--- Website/Threads/Detail.html.js
+++ Website/Threads/Detail.html.js
@@ -1,34 +1,53 @@
 ﻿(function () {
   'use strict';
 
+/*==============================================================================================================================
+| Author        Jeremy Caney (Jeremy@Caney.net)
+| Client        Skepti-Forum Project
+| Project       Facebook Archive
+\=============================================================================================================================*/
   angular
     .module('app')
     .controller('ThreadDetailController', ThreadDetailController);
 
   ThreadDetailController.$inject = ['$location', '$http'];
 
+/*==============================================================================================================================
+| CONTROLLER: THREAD DETAIL CONTROLLER
+\-----------------------------------------------------------------------------------------------------------------------------*/
+/** @ngdoc controller
+  * @name app:ThreadDetailController
+  * @description
+  * Provides data access for thread details (via archived JSON content) and additionally offers functions for filtering comments
+  * based on author and number of likes. 
+  */
   function ThreadDetailController($location, $http) {
     /* jshint validthis:true */
+
+  /*============================================================================================================================
+  | ESTABLISH VARIABLES
+  \---------------------------------------------------------------------------------------------------------------------------*/
     var vm = this;
-
     vm.post = null;
-
     vm.search = {
       from: {
         id: ''
       }
     };
-
-    vm.likedFilter = function (comment) {
-      return !vm.likedOnly || (comment.like_count && comment.like_count > 1);
-    };
-
-    vm.toggleAuthor = function () {
-      vm.search.from.id = vm.authorOnly ? vm.post.from.id : '';
-    }
+    vm.likedFilter = likedFilter;
+    vm.toggleAuthor = toggleAuthor;
 
     activate();
 
+  /*============================================================================================================================
+  | METHOD: ACTIVATE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /** @ngdoc method
+    * @name  app:ThreadDetailController#activate
+    * @kind  function
+    * @description Prepares the thread detail controller with necessary dependencies. This includes primarily the archived JSON
+    * file, which is loaded based on routing data in the URL.
+    */
     function activate() {
 
       var path = $location.path();
@@ -42,6 +61,36 @@
         }
       );
 
+    }
+
+  /*============================================================================================================================
+  | METHOD: LIKED FILTER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /** @ngdoc method
+    * @name  app:ThreadDetailController#likedFilter
+    * @kind  function
+    * @description Provides a helper function that the comment repeater can use to filter comments based on a minimum like 
+    * count (assuming the corresponding checkbox is selected).
+    * 
+    * @param {object=} comment The instant of the comment being evaluated by the repeater.
+    */
+    var likedFilter = function (comment) {
+      return !vm.likedOnly || (comment.like_count && comment.like_count > 1);
+    };
+
+  /*============================================================================================================================
+  | METHOD: TOGGLE AUTHOR
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /** @ngdoc method
+    * @name  app:ThreadDetailController#toggleAuthor
+    * @kind  function
+    * @description Provides a helper function that the comment repeater can use to filter comments based on whether or not they 
+    * were written by the author of the original post (assuming the corresponding checkbox is selected).
+    * 
+    * @param {object=} comment The instant of the comment being evaluated by the repeater.
+    */
+    var toggleAuthor = function () {
+      vm.search.from.id = vm.authorOnly ? vm.post.from.id : '';
     }
 
   }
