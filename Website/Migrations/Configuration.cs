7db70ec1ae6044174a4ee79fd76d0328df719bb9
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-07-21 18:35:04 -0700

    Reformatted seed script

--- Website/Migrations/Configuration.cs
+++ Website/Migrations/Configuration.cs
@@ -1,31 +1,29 @@
-namespace SkeptiForum.Archive.Migrations
-{
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+namespace SkeptiForum.Archive.Migrations {
 
-    internal sealed class Configuration : DbMigrationsConfiguration<SkeptiForum.Archive.Reporting.ReportingContext>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = true;
-        }
+  using System;
+  using System.Data.Entity;
+  using System.Data.Entity.Migrations;
+  using System.Linq;
+  using SkeptiForum.Archive.Reporting.Providers;
 
-        protected override void Seed(SkeptiForum.Archive.Reporting.ReportingContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+  internal sealed class Configuration : DbMigrationsConfiguration<ReportingContext> {
+    public Configuration() {
+      AutomaticMigrationsEnabled = true;
     }
+
+    protected override void Seed(ReportingContext context) {
+      //  This method will be called after migrating to the latest version.
+
+      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+      //  to avoid creating duplicate seed data. E.g.
+      //
+      //    context.People.AddOrUpdate(
+      //      p => p.FullName,
+      //      new Person { FullName = "Andrew Peters" },
+      //      new Person { FullName = "Brice Lambson" },
+      //      new Person { FullName = "Rowan Miller" }
+      //    );
+      //
+    }
+  }
 }
